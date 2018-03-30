61aa7cab55a2120a2fbc9e01e6b1660111bbd8a7
Luís Melo <luismelo7@gmail.com>
2018-03-30 10:49:35 +0100

    fixed primary key

--- prehab_app/models/Doctor.py
+++ prehab_app/models/Doctor.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from prehab_app.models.User import User
+
 
 class Doctor(models.Model):
-    id = models.AutoField(primary_key=True)
+    id = models.ForeignKey(User, on_delete=models.CASCADE, db_column='patient_type_id', primary_key=True)
     department = models.CharField(max_length=64, blank=False, null=True)
 
     class Meta:
