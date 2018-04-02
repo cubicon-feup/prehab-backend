6105d887520d412955736204a3f77b299d5b1d92
Luís Melo <luismelo7@gmail.com>
2018-04-02 21:08:08 +0100

    changed ViewSet for Tasks

--- prehab_app/serializers/TaskTypeId.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from prehab_app.models import TaskTypeId
-
-
-class TaskTypeIdSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TaskTypeId
-        fields = '__all__'
--- prehab_app/urls.py
+++ prehab_app/urls.py
@@ -1,38 +1,38 @@
 from django.conf.urls import url
 from django.urls import path, include
-from prehab_app.views.Auth import AuthView
 from rest_framework import routers
 
-from prehab_app.views.Task import TaskView
+from prehab_app.views.Auth import AuthView
+from prehab_app.views.Task import TaskViewSet
 
 router = routers.DefaultRouter()
 
+router.register(r'task', TaskViewSet, 'CRUD all over Tasks')
 
 urlpatterns = [
     url(r'login/', AuthView.as_view({'post': 'login'}), name='Login in the platform'),
-    url(r'web/register_patient/', AuthView.as_view({'post': 'register_patient'}), name='Register a new patient in the platform'),
-    url(r'task/', TaskView.as_view({'post': 'post'}), name='Create a new Task'),
+    url(r'web/register_patient/', AuthView.as_view({'post': 'register_patient'}),
+        name='Register a new patient in the platform'),
 
     path('', include(router.urls))
 ]
 
+# from django.conf.urls import url, include
+# from rest_framework.routers import DefaultRouter
 
-#from django.conf.urls import url, include
-#from rest_framework.routers import DefaultRouter
+# from prehab_app.views.Api import ApiViewSet
+# router = DefaultRouter()
 
-#from prehab_app.views.Api import ApiViewSet
-#router = DefaultRouter()
-
-#urlpatterns = [
+# urlpatterns = [
 
 #    url(r'api/',
 #        ApiViewSet.as_view({
-            #'get': 'test_endpoint',
-            # 'post': 'insert',
-            # 'put': 'update_multiple',
-            # 'delete': 'delete_multiple',
+# 'get': 'test_endpoint',
+# 'post': 'insert',
+# 'put': 'update_multiple',
+# 'delete': 'delete_multiple',
 #        }),
 #        name='Testing'),
 
 #    url(r'^', include(router.urls)),
-#]
+# ]
