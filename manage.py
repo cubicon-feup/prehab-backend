a39e4ecd6d5f15b2ab85905694796517b1d6f755
Luís Melo <luismelo7@gmail.com>
2018-04-07 22:39:24 +0100

    trying to arrenge settings (3)

--- manage.py
+++ manage.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-from .prehab.config import SETTINGS_MODULE
+from prehab.environment import SETTINGS_MODULE
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "prehab.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -13,11 +13,4 @@ if __name__ == "__main__":
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
-
-
-
-if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
-    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
--- prehab/environment.py
+++ prehab/environment.py
@@ -2,7 +2,7 @@
 ENVIRONMENT = 'development'
 # ENVIRONMENT = 'production'
 
-SETTINGS_MODULE = 'config.settings.local'
+SETTINGS_MODULE = 'settings.development'
 
 if ENVIRONMENT == 'development':
     SETTINGS_MODULE = 'settings.development'
