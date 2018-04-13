a0ffa03502d9d77292e4d6be98e5e27b3cbc4675
Luís Melo <luismelo7@gmail.com>
2018-04-13 15:21:13 +0100

    rearrenge doctor/patient creation

--- prehab_app/urls.py
+++ prehab_app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework import routers
 
 from prehab_app.views.Auth import AuthViewSet
+from prehab_app.views.Doctor import DoctorViewSet
 from prehab_app.views.FullTaskSchedule import FullTaskScheduleViewSet
 from prehab_app.views.Patient import PatientViewSet
 from prehab_app.views.Task import TaskViewSet
@@ -13,6 +14,7 @@ router = routers.DefaultRouter()
 
 router.register(r'task', TaskViewSet, 'crud-task')
 router.register(r'patient', PatientViewSet, 'crud-patient')
+router.register(r'doctor', DoctorViewSet, 'crud-doctor')
 router.register(r'user', UserViewSet, 'crud-user')
 router.register(r'schedule/task/full', FullTaskScheduleViewSet, 'crud-full-task-schedule')
 router.register(r'schedule/task', TaskScheduleViewSet, 'crud-task-schedule')
@@ -20,38 +22,5 @@ router.register(r'schedule/task', TaskScheduleViewSet, 'crud-task-schedule')
 urlpatterns = [
     url(r'login/', AuthViewSet.as_view({'post': 'login'}), name='login'),
     url(r'logout/', AuthViewSet.as_view({'post': 'logout'}), name='logout'),
-    url(r'web/register_patient/',
-        AuthViewSet.as_view({'post': 'register_patient'}),
-        name='register_new_patient'),
-    url(r'web/register_doctor',
-        AuthViewSet.as_view({'post': 'register_doctor'}),
-        name="register_new_doctor"),
-    # url(r'schedule/task/full/(?P<task_schedule_id>.+)',
-    #     TaskScheduleFullViewSet.as_view({'get': 'get_task_schedule_full'}),
-    #     name='get_task_schedule_full'),
-    # url(r'schedule/task/full/',
-    #     TaskScheduleFullViewSet.as_view({'post': 'create_task_schedule_full'}),
-    #     name='create_task_schedule_full'),
-
     path('', include(router.urls))
 ]
-
-# from django.conf.urls import url, include
-# from rest_framework.routers import DefaultRouter
-
-# from prehab_app.views.Api import ApiViewSet
-# router = DefaultRouter()
-
-# urlpatterns = [
-
-#    url(r'api/',
-#        ApiViewSet.as_view({
-# 'get': 'test_endpoint',
-# 'post': 'insert',
-# 'put': 'update_multiple',
-# 'delete': 'delete_multiple',
-#        }),
-#        name='Testing'),
-
-#    url(r'^', include(router.urls)),
-# ]
--- prehab_app/views/Patient.py
+++ prehab_app/views/Patient.py
@@ -1,7 +1,14 @@
+import string
+from datetime import datetime
+import random
+
 from rest_framework.viewsets import GenericViewSet
 
 from prehab.helpers.HttpException import HttpException
 from prehab.helpers.HttpResponseHandler import HTTP
+from prehab.helpers.SchemaValidator import SchemaValidator
+from prehab.permissions import Permission
+from prehab_app.models import ConstraintType, PatientConstraintType, Doctor, Role, User
 from prehab_app.models.DoctorPatient import DoctorPatient
 from prehab_app.models.Patient import Patient
 from prehab_app.serializers.Patient import PatientSerializer
@@ -58,40 +65,71 @@ class PatientViewSet(GenericViewSet):
 
     @staticmethod
     def create(request):
-        return HTTP.response(405, '')
-        # if not Permission.verify(request, ['Doctor']):
-        #     raise HttpException(401)
-        #
-        # try:
-        #     data = request.data
-        #     # 1. Check if task_type is not null
-        #     if 'task_type_id' not in data:
-        #         raise HttpException(400, 'You need to send a task_type_id.')
-        #
-        #     # 2. Check if task type is available
-        #     task_type = TaskType.objects.task_type(data['task_type_id'])
-        #
-        #     if len(task_type) == 0:
-        #         raise HttpException(400, 'Task Type does not exist.')
-        #
-        #     new_task = Task(
-        #         title=data['title'],
-        #         description=data.get('description', None),
-        #         multimedia_link=data.get('multimedia_link', None),
-        #         task_type=task_type.get()
-        #     )
-        #     new_task.save()
-        #
-        # except HttpException as e:
-        #     return HTTP.response(e.http_code, e.http_detail)
-        # except Exception as e:
-        #     return HTTP.response(400, str(e))
-        #
-        # # Send Response
-        # data = {
-        #     'task_id': new_task.id
-        # }
-        # return HTTP.response(201, '', data)
+        try:
+            # 0 - Handle Permissions
+            if not Permission.verify(request, ['Doctor']):
+                raise HttpException(401)
+
+            data = request.data
+            # 1. Check schema
+            SchemaValidator.validate_obj_structure(data, 'patient/create.json')
+
+            # 2. Add new User
+            new_user = User(
+                name='Anónimo',
+                username='',
+                email=data['email'] if 'email' in data else None,
+                phone=data['phone'] if 'phone' in data else None,
+                password=None,
+                role=Role.objects.patient_role().get(),
+                activation_code=''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8)),
+                is_active=False,
+            )
+            new_user.save()
+            # 1. Generate Activation Code & Username
+            patient_tag = "HSJ{}{}".format(datetime.now().year, str(new_user.id).zfill(4))
+            new_user.username = patient_tag
+            new_user.save()
+            doctor = Doctor.objects.get(id=request.USER_ID)
+
+            # 3. Add new Patient
+            new_patient = Patient(
+                id=new_user,
+                patient_tag=patient_tag,
+                age=data['age'],
+                height=data['height'],
+                weight=data['weight'],
+                sex=data['sex']
+            )
+            new_patient.save()
+
+            # 4. Create Doctor Patient Association
+            DoctorPatient(
+                patient=new_patient,
+                doctor=doctor
+            )
+
+            # 5. Associate Constraints
+            for constraint_id in data['constraints']:
+                constraint_type = ConstraintType.objects.get(id=constraint_id)
+                PatientConstraintType(
+                    patient=new_patient,
+                    constraint_type=constraint_type
+                )
+
+            # 6. Generate Task Plan - TODO
+            # TODO
+        except HttpException as e:
+            return HTTP.response(e.http_code, e.http_detail)
+        except Exception as e:
+            return HTTP.response(400, str(e))
+
+        # Send Response - access code
+        data = {
+            'access_code': new_user.activation_code
+        }
+        return HTTP.response(200, '', data)
+
 
     @staticmethod
     def update(request, pk=None):
