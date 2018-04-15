f832c87fa74c997677588ee98274ae1f5d0e2d10
Luís Melo <luismelo7@gmail.com>
2018-04-15 16:48:52 +0100

    Feature/get prehab plan for patient (#37)
    * missing validations
    
    * fix generation algorithm
    
    * added validation of user in accessing the prehab plan

--- prehab_app/models/PatientTaskSchedule.py
+++ prehab_app/models/PatientTaskSchedule.py
@@ -11,10 +11,10 @@ class PatientTaskScheduleQuerySet(models.QuerySet):
 
 class PatientTaskSchedule(models.Model):
     id = models.AutoField(primary_key=True)
-    prehab = models.ForeignKey(Prehab, on_delete=models.CASCADE, db_column='prehab_id')
+    prehab = models.ForeignKey(Prehab, on_delete=models.CASCADE, db_column='prehab_id', related_name='patient_task_schedule')
     week_number = models.IntegerField(blank=False, null=False)
     day_number = models.IntegerField(blank=False, null=False)
-    task = models.ForeignKey(Task, on_delete=models.CASCADE, db_column='task_id')
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, db_column='task_id', related_name='patient_task')
     expected_repetitions = models.IntegerField(blank=False, null=True)
     actual_repetitions = models.IntegerField(blank=False, null=True)
     status = models.ForeignKey(PatientTaskScheduleStatus, on_delete=models.CASCADE, db_column='status_id')
--- prehab_app/serializers/Prehab.py
+++ prehab_app/serializers/Prehab.py
@@ -1,9 +1,49 @@
 from rest_framework import serializers
-
+import datetime
 from prehab_app.models.Prehab import Prehab
+from prehab_app.serializers.PatientTaskSchedule import PatientTaskScheduleSerializer
+from prehab_app.serializers.Task import TaskSerializer
 
 
 class PrehabSerializer(serializers.ModelSerializer):
     class Meta:
         model = Prehab
         fields = '__all__'
+
+
+class FullPrehabSerializer(serializers.ModelSerializer):
+    patient_task_schedule = PatientTaskScheduleSerializer(many=True, read_only=True)
+    task_schedule = serializers.SerializerMethodField()
+
+    def to_representation(self, obj):
+        data = super(FullPrehabSerializer, self).to_representation(obj)  # the original data
+
+        # data['task']['expected_repetitions'] = data['expected_repetitions']
+        del data['patient_task_schedule']
+
+        return data
+
+    @staticmethod
+    def setup_eager_loading(queryset):
+        """ Perform necessary eager loading of data. """
+        queryset = queryset.prefetch_related('patient_task_schedule', 'patient_task_schedule__task')
+        return queryset
+
+    class Meta:
+        model = Prehab
+        fields = '__all__'
+
+    @staticmethod
+    def get_task_schedule(obj):
+        task_schedule = {}
+        for task in obj.patient_task_schedule.all():
+            date = str(obj.init_date + datetime.timedelta(days=7*(task.week_number - 1)) + datetime.timedelta(days=(task.day_number - 1)))
+
+            if date not in task_schedule:
+                task_schedule[date] = []
+
+            task_info = TaskSerializer(task.task, many=False).data
+            task_info['status'] = task.status.title
+            task_schedule[date].append(task_info)
+
+        return task_schedule
