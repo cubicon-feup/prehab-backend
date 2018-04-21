538a3495063970522e066e1fab53c49e5060fd3f
Luís Melo <luismelo7@gmail.com>
2018-04-21 13:34:41 +0100

    added role names to list/retrieve doctors

--- prehab_app/serializers/User.py
+++ prehab_app/serializers/User.py
@@ -1,15 +1,49 @@
 from rest_framework import serializers
 
 from prehab_app.models.User import User
+from prehab_app.serializers.UserType import RoleSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer(many=False, read_only=True)
+
+    def to_representation(self, obj):
+        data = super(UserSerializer, self).to_representation(obj)  # the original data
+
+        data['role_id'] = data['role']['id']
+        data['role_name'] = data['role']['title']
+        del data['role']
+
+        return data
+
+    @staticmethod
+    def setup_eager_loading(queryset):
+        """ Perform necessary eager loading of data. """
+        queryset = queryset.prefetch_related('role')
+        return queryset
+
     class Meta:
         model = User
         fields = '__all__'
 
 
 class SimpleUserSerializer(serializers.ModelSerializer):
+    role = RoleSerializer(many=False, read_only=True)
+
+    def to_representation(self, obj):
+        data = super(SimpleUserSerializer, self).to_representation(obj)  # the original data
+
+        data['role_id'] = data['role']['id']
+        data['role_name'] = data['role']['title']
+        del data['role']
+
+        return data
+
+    @staticmethod
+    def setup_eager_loading(queryset):
+        """ Perform necessary eager loading of data. """
+        queryset = queryset.prefetch_related('role')
+        return queryset
 
     class Meta:
         model = User
