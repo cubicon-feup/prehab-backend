d9d62a94c48f5db32d3679e162666c0783b37ab5
Luís Melo <luismelo7@gmail.com>
2018-05-01 18:23:45 +0100

    removed task info

--- prehab_app/serializers/PatientTaskInfo.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from prehab_app.models.PatientTaskInfo import PatientTaskInfo
-
-
-class PatientTaskInfoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PatientTaskInfo
-        fields = '__all__'
