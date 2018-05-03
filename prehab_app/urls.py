1f0b39c9cc64129842b218b5d48bff5d29481f53
Luís Melo <luismelo7@gmail.com>
2018-05-03 12:49:47 +0100

    added meal routes

--- prehab_app/urls.py
+++ prehab_app/urls.py
@@ -5,6 +5,7 @@ from rest_framework import routers
 from prehab_app.views.Auth import AuthViewSet
 from prehab_app.views.Doctor import DoctorViewSet
 from prehab_app.views.FullTaskSchedule import FullTaskScheduleViewSet
+from prehab_app.views.Meal import MealViewSet
 from prehab_app.views.Patient import PatientViewSet
 from prehab_app.views.PatientTaskSchedule import PatientTaskScheduleViewSet
 from prehab_app.views.Prehab import PrehabViewSet
@@ -22,6 +23,7 @@ router.register(r'user', UserViewSet, 'crud-user')
 router.register(r'prehab', PrehabViewSet, 'crud-prehab')
 router.register(r'schedule/task/full', FullTaskScheduleViewSet, 'crud-full-task-schedule')
 router.register(r'schedule/task', TaskScheduleViewSet, 'crud-task-schedule')
+router.register(r'meal', MealViewSet, 'crud-meal')
 
 urlpatterns = [
     url(r'login/', AuthViewSet.as_view({'post': 'login'}), name='login'),
